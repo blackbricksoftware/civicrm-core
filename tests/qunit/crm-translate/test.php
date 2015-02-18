d48f04a5a7f6a635ce112625d5467843519a646a
David Hayes <david@blackbricksoftware.com>
2015-02-18 10:12:06 -0600

    remove unecessary files

--- tests/qunit/crm-translate/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-CRM_Core_Resources::singleton()->addSetting(array(
-    'strings' => array('One, two, three' => 'Un, deux, trois')
-  )
-);
-// CRM_Core_Resources::singleton()->addScriptFile(...);
-// CRM_Core_Resources::singleton()->addStyleFile(...);
-// CRM_Core_Resources::singleton()->addSetting(...);
